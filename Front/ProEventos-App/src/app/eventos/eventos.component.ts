d2021cb2ce537c2fdd14e9ae06527c6fb27ce848
Julio Cezar Boaventura Novais <julionovais20@hotmail.com>
2022-05-30 07:02:45 -0300

    Sessão 5

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -10,16 +10,53 @@ export class EventosComponent implements OnInit {
 
   public eventos: any = [];
 
+  widthImg: number = 150;
+  marginImg: number = 2;
+  exibirImagem: boolean = true;
+  private _filtroLista: string = '';
+  public eventosFiltrados: any = [];
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    console.log(this.filtroLista);
+    this.eventosFiltrados = this._filtroLista ? this.filtrarEventos(this._filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string}) =>
+        evento.tema.toLocaleLowerCase().indexOf (filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+
   constructor(private http: HttpClient) {
   }
 
-  ngOnInit(): void {
-    this.geteventos();
+  alterarImagem() {
+    this.exibirImagem = !this.exibirImagem;
   }
 
-  public geteventos(): void {
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getFoto(): string {
+    console.log("\\assets\\" + this.eventos[0].imagemURL);
+    return "\\assets\\" + this.eventos[0].imagemURL;
+  }
+
+  public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response
+        this.eventosFiltrados = response;
+      },
       error => console.log(error)
     );
     //  this.eventos = [
@@ -28,6 +65,8 @@ export class EventosComponent implements OnInit {
     //    { tema: 'C#'     , local: 'Minas'    }
     //  ];
   }
+
+
 
 
 }
